11d9e8c74726d16d4df216e4a8ed92fd6d17819b
Lucas Gomes Santiago <lucasgomessantiago@gmail.com>
2020-12-28 19:32:25 -0300

    implementando test em cliente controller

--- avaliacao/src/test/java/com/petz/avaliacao/controllers/ClienteControllerTest.java
+++ avaliacao/src/test/java/com/petz/avaliacao/controllers/ClienteControllerTest.java
@@ -1,4 +1,60 @@
 package com.petz.avaliacao.controllers;
 
+import com.petz.avaliacao.application.commands.cliente.CriarClienteCommand;
+import com.petz.avaliacao.application.services.ClienteService;
+import com.petz.avaliacao.presentation.controllers.ClienteController;
+import com.petz.avaliacao.wrapers.ClienteAlreadyRegisteredException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static com.petz.avaliacao.utils.JsonConvertionUtils.asJsonString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
 public class ClienteControllerTest {
+
+    private static final String RESOURCE_API_URL_PATH = "/api/v1/clientes";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private ClienteService clienteService;
+
+    @InjectMocks
+    private ClienteController clienteController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(clienteController)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+    @Test
+    void criar_Cliente_Post_Com_Sucesso() throws Exception {
+        // given
+        var command = new CriarClienteCommand("cliente","teste@gmail.com");
+
+        // when
+        doNothing().when(clienteService).adicionarCliente(any());
+
+        // then
+        mockMvc.perform(post(RESOURCE_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(command)))
+                .andExpect(status().isCreated());
+    }
 }
